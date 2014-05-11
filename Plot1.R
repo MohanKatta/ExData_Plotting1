be248ffc2cbad3ed022ddefc5ca94c68dafc1742
Mohan Krishna Katta <kmkrishnamba@gmail.com>
2014-05-11 10:18:34 +0530

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,12 @@
+setwd("C:\\coursera\\expdataanalysis\\gitclone\\ExData_Plotting1")
+cp1 <- read.table("household_power_consumption.txt",header=TRUE,na.strings="?",sep=";")
+head(cp1)
+
+cp1a <- cp1[as.Date(cp1$Date,format="%d/%m/%Y")>="2007-02-01"&as.Date(cp1$Date,format="%d/%m/%Y")<="2007-02-02",]
+#We will only be using data from the dates 2007-02-01 and 2007-02-02.
+
+png(filename = "Plot1.png",
+    width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white")
+hist(cp1a$Global_active_power,main="Global Active Power",col="red",xlab="Global Active Power (kilowatts)")
+dev.off()
